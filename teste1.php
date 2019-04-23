748875d61e3c2f9e4c738d487aaa5a6030463337
Erick Costa <erick.costa.ti@gmail.com>
2019-04-22 22:41:24 -0300

    Basic PHP concepts

---
+++ teste1.php
@@ -0,0 +1,23 @@
+
+<h1> H1 title</h1>
+
+<p> Teste </p>
+
+<?php
+
+echo "Hello! \n";
+
+// ... mais código
+
+echo "My first app! \n";
+
+// ... o script acaba aqui
+
+
+if (1 < 2) {
+    echo "<h1>Teste<h1/>";
+}else {
+    echo "It is small";
+}
+
+?>
